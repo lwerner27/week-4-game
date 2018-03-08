532eea213f85e576c0408ba9aeba341632decb1e
lwerner27 <lwerner27@gmail.com>
2018-03-07 21:27:16 -0600

    Gameplay is mostly finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
     //change this 2 after player one choses, switch to 3 after choosing first opponent then change back to 2 after a defeat
     let whichPlayer = 1 
+    // Designates the player pokemon
+    let playerPokemon;
+    // Designates the computer pokemon
+    let computerPokemon;
     // Targets all the pokemond divs
     let pokemonDiv = $('.pokemon-div')
     // Targets Pikachu's div
@@ -13,57 +17,134 @@ $(document).ready(function() {
     // Targets Venusaur div
     let venusaurDiv = $('#Venusaur')
     // Targets the your-pokemon div
-    let yourPokemon = $('#your-pokemon')
+    let yourPokemonDiv = $('#your-pokemon')
     // Targets the opposing-pokemon div
-    let opposingPokemon = $('#opposing-pokemon')
+    let opposingPokemonDiv = $('#opposing-pokemon')
+    // Targets the attack-button
+    let attackButton = $('#attack-button')
+    // Pikachu Object
+    let pikachu = {
+        name: "Pikachu",
+        originalHealth: 200,
+        health: 200,
+        originalAttack: 50,
+        attack: 50,
+        counterAttack: 75,
+        healthElement: $('#pikachu-health')
+    }
+    // Charizard Object
+    let charizard = {
+        name: "Charizard",
+        originalHealth: 200,
+        health: 200,
+        originalAttack: 50,
+        attack: 50,
+        counterAttack: 100,
+        healthElement: $('#charizard-health')
+    }
+    // Blastoise Object
+    let blastoise = {
+        name: "Blastoise",
+        originalHealth: 200,
+        health: 200,
+        originalAttack: 50,
+        attack: 50,
+        counterAttack: 50,
+        healthElement: $('#blastoise-health')
+    }
+    // Venusaur Object
+    let venusaur = {
+        name: "Venusaur",
+        originalHealth: 200,
+        health: 200,
+        originalAttack: 50,
+        attack: 50,
+        counterAttack: 25,
+        healthElement: $('#venusaur-health')
+    }
 
     // Function Declarations
+
+    // Chooses the pokemon for you and your opponent
     function choosePokemon (arg1) {
         if (whichPlayer === 1) {
             if (arg1 === 'Pikachu') {
+                playerPokemon = pikachu
                 pikachuDiv.removeClass('m4')
                 pikachuDiv.addClass('m12')
-                yourPokemon.append(pikachuDiv);
+                yourPokemonDiv.append(pikachuDiv);
+                console.log(playerPokemon)
                 whichPlayer = 2
             } else if (arg1 === 'Charizard') {
+                playerPokemon = charizard
                 charizardDiv.removeClass('m4')
                 charizardDiv.addClass('m12')
-                yourPokemon.append(charizardDiv)
+                yourPokemonDiv.append(charizardDiv)
+                console.log(playerPokemon)
                 whichPlayer = 2
             } else if (arg1 === 'Blastoise') {
+                playerPokemon = blastoise
                 blastoiseDiv.removeClass('m4')
                 blastoiseDiv.addClass('m12')
-                yourPokemon.append(blastoiseDiv)
+                yourPokemonDiv.append(blastoiseDiv)
+                console.log(playerPokemon)
                 whichPlayer = 2
             } else if (arg1 === 'Venusaur') {
+                playerPokemon = venusaur
                 venusaurDiv.removeClass('m4')
                 venusaurDiv.addClass('m12')
-                yourPokemon.append(venusaurDiv)
+                yourPokemonDiv.append(venusaurDiv)
+                console.log(playerPokemon)
                 whichPlayer = 2
             }
         } else if (whichPlayer === 2) {
             if (arg1 === 'Pikachu') {
+                computerPokemon = pikachu
                 pikachuDiv.removeClass('m4')
                 pikachuDiv.addClass('m12')
-                opposingPokemon.append(pikachuDiv);
+                opposingPokemonDiv.append(pikachuDiv);
+                console.log(computerPokemon)
                 whichPlayer = 3
             } else if (arg1 === 'Charizard') {
+                computerPokemon = charizard
                 charizardDiv.removeClass('m4')
                 charizardDiv.addClass('m12')
-                opposingPokemon.append(charizardDiv)
+                opposingPokemonDiv.append(charizardDiv)
+                console.log(computerPokemon)
                 whichPlayer = 3
             } else if (arg1 === 'Blastoise') {
+                computerPokemon = blastoise
                 blastoiseDiv.removeClass('m4')
                 blastoiseDiv.addClass('m12')
-                opposingPokemon.append(blastoiseDiv)
+                opposingPokemonDiv.append(blastoiseDiv)
+                console.log(computerPokemon)
                 whichPlayer = 3
             } else if (arg1 === 'Venusaur') {
+                computerPokemon = venusaur
                 venusaurDiv.removeClass('m4')
                 venusaurDiv.addClass('m12')
-                opposingPokemon.append(venusaurDiv)
+                opposingPokemonDiv.append(venusaurDiv)
+                console.log(computerPokemon)
                 whichPlayer = 3
             }
         }
+    }
+
+    // Does the battle calculations
+    function doBattle() {
+
+        computerPokemon.health = computerPokemon.health - playerPokemon.attack
+        
+        if (computerPokemon.health > 0) {
+            playerPokemon.health = playerPokemon.health - computerPokemon.counterAttack
+            playerPokemon.healthElement.html(playerPokemon.health)
+            computerPokemon.healthElement.html(computerPokemon.health)
+        } else if (computerPokemon.health <= 0) {
+            opposingPokemonDiv.empty();
+            whichPlayer = 2
+        }
+        
+
     }
 
     // On click function
@@ -71,6 +152,17 @@ $(document).ready(function() {
         let clickedPokemon = this.id
         choosePokemon(clickedPokemon)
     })
+
+    attackButton.on('click', function () {
+        if (playerPokemon.health > 0) {
+            doBattle()
+        } else {
+            yourPokemonDiv.empty()
+            opposingPokemonDiv.empty()
+        }
+    })
+
+
 
 });
 
